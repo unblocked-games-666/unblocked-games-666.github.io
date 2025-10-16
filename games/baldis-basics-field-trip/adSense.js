e0de60d0055e5afbb7a3371e71ce276860633d56
retro-bowls-unblocked <erginozkan222@yandex.com>
2025-10-16 20:30:00 +0000

    Remove games folder and rename zips to game_archives

--- games/baldis-basics-field-trip/adSense.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    let script = document.createElement("script");
-    script.async = true;
-    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7233485374797646";
-    script.crossOrigin = "anonymous";
-    document.head.appendChild(script);
-})();
