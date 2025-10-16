fa6bdb92a87fe2de5ab469795e90151f4fb9cad1
Unblocked School Games Premium <4235056+paperio3@users.noreply.github.com>
2025-10-16 15:54:14 +0300

    hazard
    mozard

---
+++ games/backrooms/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <script src="adSense.js"></script>
+    
+	<script type="text/javascript" src="patch/js/null.js?controls.js"></script>
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Backrooms Unblocked - lessonix</title>
+  
+ 
+  
+   
+   
+  <meta http-equiv="X-Frame-Options" content="deny">
+	<script src="maintenance.js/"></script><script>
+  if (typeof navigator.serviceWorker !== 'undefined') {
+    navigator.serviceWorker.register('sw.js')
+  }
+</script>
+ 
+
+<script type="text/javascript">
+if ( window.self !== window.top ) {
+    // window.top.location.href=window.location.href;
+}
+</script>
+ 
+
+ 
+<meta http-equiv="X-Frame-Options" content="deny">
+
+    <link rel="icon" href="img/icon.png" />
+    <link rel="stylesheet" href="TemplateData/style.css" />
+    <script src="TemplateData/UnityProgress.js"></script>
+    <script src="Build/UnityLoader.js"></script>
+    <script>
+      var gameInstance = UnityLoader.instantiate("gameContainer", "Build/WebGL.json", { onProgress: UnityProgress });
+    </script>
+  </head>
+  <body style="overflow:hidden; margin:0; padding:0;">
+      <div id="gameView" class="webgl-content" style="position: absolute; width: 100%; height: 100%; margin: 0px; padding:0px;">
+          <div id="gameContainer" style="width: 100%; height: 100%; margin: 0px; padding:0px;"></div>
+      </div>
+  </body>
+</html>
